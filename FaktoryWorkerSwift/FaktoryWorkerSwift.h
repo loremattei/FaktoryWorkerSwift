e1c04c5af638c95987c78d1aba88f8f4fc1705d1
Lorenzo Mattei <lore@hugin.hugo-home>
2017-11-17 10:24:27 +0100

    Initial Commit

---
+++ FaktoryWorkerSwift/FaktoryWorkerSwift.h
@@ -0,0 +1,19 @@
+//
+//  FaktoryWorkerSwift.h
+//  FaktoryWorkerSwift
+//
+//  Created by Lorenzo Mattei on 17/11/2017.
+//  Copyright © 2017 Lorenzo Mattei. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for FaktoryWorkerSwift.
+FOUNDATION_EXPORT double FaktoryWorkerSwiftVersionNumber;
+
+//! Project version string for FaktoryWorkerSwift.
+FOUNDATION_EXPORT const unsigned char FaktoryWorkerSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FaktoryWorkerSwift/PublicHeader.h>
+
+
